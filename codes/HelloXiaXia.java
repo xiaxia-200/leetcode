8da667b3421b4db08ccd2ec2d1cdb40549662a6e
xiaxia-200 <1158996489@qq.com>
2020-10-27 10:41:31 +0800

    ^X

---
+++ codes/HelloXiaXia.java
@@ -0,0 +1,25 @@
+public class HelloXiaXia {
+    // public int findPoisonedDuration(int[] timeSeries, int duration) {
+    //     int totalDuration=0;
+    //     for(int i=0; i < timeSeries.length-1; i++){
+    //         int j = i + 1;
+    //         int m = timeSeries[j]-timeSeries[i];
+    //         if(m < duration){
+    //             totalDuration+=m;
+    //         }
+    //         else{
+    //             totalDuration+=duration;
+    //         }
+    //     }
+    //     if(timeSeries.length > 0){
+    //         totalDuration+=duration;
+    //     }
+    //     return totalDuration;
+    // }
+    public static void main(String[] args) {
+        int a = 2;
+        a++;
+        a = 3;
+        System.out.println("Hello Xiaxia!");
+    }
+}
